1a9a7a798f064017a96f98a48396b67864151502
Anas Ocyber <anas@ocyber.work>
2024-05-13 18:16:19 +0500

    Build responsive home layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import FileInput from "./components/FileInput";
+import FilesList from "./components/FilesList";
+import apiClient from "./services/api-client";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  const [fileName, setFileName] = useState("");
+  const [error, setError] = useState("");
+  const [filesList, setFilesList] = useState<string[]>([]);
+
+  useEffect(() => {
+    async function fetchFiles() {
+      const { data, error } = await apiClient.storage.from("asset").list();
+
+      if (error) setError(error.message);
+      else {
+        const fileList = data?.map(
+          (file: any) => !file.name.startsWith(".") && file.name
+        );
+        setFilesList(fileList);
+      }
+    }
+
+    fetchFiles();
+  }, [fileName]);
+
+  return (
+    <Grid
+      templateAreas={{ sm: `"form" "files"`, lg: `"form files"` }}
+      padding="10px"
+    >
+      <GridItem area="form">
+        <FileInput onUpload={(fileName) => setFileName(fileName)} />
+      </GridItem>
+      <GridItem area="files">
+        {error && <Text color="red">{error}</Text>}
+        <FilesList filesList={filesList} />
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
