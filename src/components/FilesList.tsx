42dcc15d00b295f634bb55635140ecb81fef8b22
Anas Ocyber <anas@ocyber.work>
2024-05-14 12:14:38 +0500

    Add delete file functionality

--- src/components/FilesList.tsx
+++ src/components/FilesList.tsx
@@ -1,4 +1,14 @@
-import { OrderedList, ListItem, Image, HStack, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Image,
+  Table,
+  TableContainer,
+  Td,
+  Text,
+  Tr,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
 interface Props {
@@ -6,29 +16,51 @@ interface Props {
 }
 
 const FilesList = ({ filesList }: Props) => {
+  const [files, setFiles] = useState<string[]>(filesList);
+
+  useEffect(() => {
+    setFiles(filesList);
+  }, [filesList]);
+
+  const handleClick = async (file: string) => {
+    const isConfirmed = confirm("Are you sure you want to delete this file?");
+    if (isConfirmed) {
+      const { error } = await apiClient.storage.from("asset").remove([file]);
+      if (!error) {
+        setFiles((prevFiles) => prevFiles.filter((f) => f !== file));
+      } else {
+        console.error("Error deleting file:", error);
+      }
+    }
+  };
+
   return (
-    <OrderedList
-      fontSize="18px"
-      padding="10px"
-      listStyleType="none"
-      spacing="24px"
-    >
-      {filesList?.map((file, index) => (
-        <ListItem key={index}>
-          <HStack>
-            <Image
-              boxSize="100px"
-              objectFit="cover"
-              src={
-                apiClient.storage.from("asset").getPublicUrl(file).data
-                  .publicUrl
-              }
-            />
-            <Text>{file}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </OrderedList>
+    <TableContainer>
+      <Table>
+        {files?.map((file, index) => (
+          <Tr key={index}>
+            <Td>
+              <Flex justifyContent="space-between" alignItems="center">
+                <Flex columnGap="10px" alignItems="center">
+                  <Image
+                    boxSize="80px"
+                    objectFit="cover"
+                    src={
+                      apiClient.storage.from("asset").getPublicUrl(file).data
+                        .publicUrl
+                    }
+                  />
+                  <Text>{file}</Text>
+                </Flex>
+                <Button colorScheme="red" onClick={() => handleClick(file)}>
+                  Delete
+                </Button>
+              </Flex>
+            </Td>
+          </Tr>
+        ))}
+      </Table>
+    </TableContainer>
   );
 };
 
